70d07947667b5d9ccb99d47fefa70bb0a0593b01
mkadr5 <muhammetkadir52@gmail.com>
2023-03-18 19:39:03 +0300

    Completed

--- CryptoCrazy/Service/WebService.swift
+++ CryptoCrazy/Service/WebService.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+class WebService{
+    func downloadCurrencies(url:URL, completion: @escaping ([CryptoCurrency]?) -> ()){
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error{
+                print(error.localizedDescription ?? "Error")
+                completion(nil)
+            }else if let data = data{
+                let cryptoList = try? JSONDecoder().decode([CryptoCurrency].self, from: data)
+                print(cryptoList)
+                
+                if let cryptoList = cryptoList{
+                    completion(cryptoList)
+                }
+            }
+        }.resume()
+    }
+}
